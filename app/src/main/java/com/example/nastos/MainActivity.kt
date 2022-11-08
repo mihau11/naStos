9ac7ba9905a3af1bc93bbc2d41e8a176c42fe4ef
budzik12 <81246476+budzik12@users.noreply.github.com>
2022-11-08 10:32:16 +0100

    kolejki

--- app/src/main/java/com/example/nastos/MainActivity.kt
+++ app/src/main/java/com/example/nastos/MainActivity.kt
@@ -11,6 +11,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val stosik = arrayOf(1,"dwa",'3')
+        val kolejeczka = arrayOf(1,"dwa",'3')
         var stos = findViewById<TextView>(R.id.stosView)
         var kolejka = findViewById<TextView>(R.id.queueWiew)
         //podgląd
@@ -29,5 +30,21 @@ class MainActivity : AppCompatActivity() {
             stos.text="Usunięcie element nr. "+(stosik.size-1).toString()+" o wartości "+findViewById<TextView>(R.id.textView).text.toString()
             stosik.reversed().drop(1)
                     }
-
+                    //KOLEJKA
+        //podgląd
+        findViewById<Button>(R.id.button7).setOnClickListener {
+            kolejka.text="Wartości kolejki: "
+            for (i in kolejeczka){kolejka.setText(kolejka.text.toString()+i.toString())}
+        }//wypisanie długości
+        findViewById<Button>(R.id.button9).setOnClickListener {
+            kolejka.text="Wysokość stosu: "+kolejeczka.size.toString()
+        }//dodanie elementu
+        findViewById<Button>(R.id.button6).setOnClickListener {
+            kolejka.text="Dodano element nr. "+kolejeczka.size.toString()+" o wartości "+findViewById<TextView>(R.id.textView).text.toString()
+            kolejeczka[kolejeczka.size]=findViewById<TextView>(R.id.textView).text.toString()
+        }//usunięcie elementu
+        findViewById<Button>(R.id.button8).setOnClickListener {
+            kolejka.text="Usunięcie element nr. "+(kolejeczka.size-1).toString()+" o wartości "+findViewById<TextView>(R.id.textView).text.toString()
+            kolejeczka.drop(1)
+        }
 }}
