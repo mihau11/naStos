7c83947b85e32291a30cce2120c13ddcc5ca113d
budzik12 <81246476+budzik12@users.noreply.github.com>
2022-11-08 11:19:12 +0100

    KONIEC

--- app/src/main/java/com/example/nastos/MainActivity.kt
+++ app/src/main/java/com/example/nastos/MainActivity.kt
@@ -10,41 +10,42 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val stosik = arrayOf(1,"dwa",'3')
-        val kolejeczka = arrayOf(1,"dwa",'3')
+        var stosik = arrayOf(1,"dwa",'3')
+        var kolejeczka = arrayOf(1,"dwa",'3')
         var stos = findViewById<TextView>(R.id.stosView)
         var kolejka = findViewById<TextView>(R.id.queueWiew)
         //podgląd
         findViewById<Button>(R.id.button5).setOnClickListener {
             stos.text="Wartości stosu"
-            for (i in stosik){stos.setText(stos.text.toString()+i.toString())}
+            for (i in stosik){stos.setText(stos.text.toString()+"\n"+i.toString())}
     }//wypisanie długości
         findViewById<Button>(R.id.button2).setOnClickListener {
             stos.text="Wysokość stosu: "+stosik.size.toString()
         }//dodanie elementu
         findViewById<Button>(R.id.button).setOnClickListener {
             stos.text="Dodano element nr. "+stosik.size.toString()+" o wartości "+findViewById<TextView>(R.id.textView).text.toString()
-            stosik[stosik.size]=findViewById<TextView>(R.id.textView).text.toString()
+            stosik=stosik.plus(findViewById<TextView>(R.id.textView).text.toString())
         }//usunięcie elementu
         findViewById<Button>(R.id.button4).setOnClickListener {
-            stos.text="Usunięcie element nr. "+(stosik.size-1).toString()+" o wartości "+findViewById<TextView>(R.id.textView).text.toString()
-            stosik.reversed().drop(1)
+            stos.text="Usunięcie element nr. "+(stosik.size-1).toString()+" o wartości "+stosik[stosik.size-1].toString()
+            stosik=stosik.reversed().drop(1).toTypedArray()
+            stosik.reverse()
                     }
                     //KOLEJKA
         //podgląd
         findViewById<Button>(R.id.button7).setOnClickListener {
             kolejka.text="Wartości kolejki: "
-            for (i in kolejeczka){kolejka.setText(kolejka.text.toString()+i.toString())}
+            for (i in kolejeczka){kolejka.setText(kolejka.text.toString()+"\n"+i.toString())}
         }//wypisanie długości
         findViewById<Button>(R.id.button9).setOnClickListener {
             kolejka.text="Wysokość stosu: "+kolejeczka.size.toString()
         }//dodanie elementu
         findViewById<Button>(R.id.button6).setOnClickListener {
             kolejka.text="Dodano element nr. "+kolejeczka.size.toString()+" o wartości "+findViewById<TextView>(R.id.textView).text.toString()
-            kolejeczka[kolejeczka.size]=findViewById<TextView>(R.id.textView).text.toString()
+            kolejeczka=kolejeczka.plus(findViewById<TextView>(R.id.textView).text.toString())
         }//usunięcie elementu
         findViewById<Button>(R.id.button8).setOnClickListener {
-            kolejka.text="Usunięcie element nr. "+(kolejeczka.size-1).toString()+" o wartości "+findViewById<TextView>(R.id.textView).text.toString()
-            kolejeczka.drop(1)
+            kolejka.text="Usunięcie element nr. "+(kolejeczka.size-1).toString()+" o wartości "+kolejeczka[kolejeczka.size-1].toString()
+            kolejeczka=kolejeczka.drop(1).toTypedArray()
         }
 }}
