30d2fe947e99771939f9f7f4950fd13670f30526
budzik12 <81246476+budzik12@users.noreply.github.com>
2022-11-08 10:26:32 +0100

    caly stos, ale nie dziala

--- app/src/main/java/com/example/nastos/MainActivity.kt
+++ app/src/main/java/com/example/nastos/MainActivity.kt
@@ -10,12 +10,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var stosik = arrayOf(1,2,3,4,5)
+        val stosik = arrayOf(1,"dwa",'3')
         var stos = findViewById<TextView>(R.id.stosView)
         var kolejka = findViewById<TextView>(R.id.queueWiew)
         //podgląd
         findViewById<Button>(R.id.button5).setOnClickListener {
             stos.text="Wartości stosu"
             for (i in stosik){stos.setText(stos.text.toString()+i.toString())}
-    }
+    }//wypisanie długości
+        findViewById<Button>(R.id.button2).setOnClickListener {
+            stos.text="Wysokość stosu: "+stosik.size.toString()
+        }//dodanie elementu
+        findViewById<Button>(R.id.button).setOnClickListener {
+            stos.text="Dodano element nr. "+stosik.size.toString()+" o wartości "+findViewById<TextView>(R.id.textView).text.toString()
+            stosik[stosik.size]=findViewById<TextView>(R.id.textView).text.toString()
+        }//usunięcie elementu
+        findViewById<Button>(R.id.button4).setOnClickListener {
+            stos.text="Usunięcie element nr. "+(stosik.size-1).toString()+" o wartości "+findViewById<TextView>(R.id.textView).text.toString()
+            stosik.reversed().drop(1)
+                    }
+
 }}
